faf7f00cdbc6fc10454c26c3ba4223a855bda4b3
zoechi <gzoechi@gmail.com>
2014-06-18 11:25:00 +0200

    export bwu_utils

---
+++ lib/math/math.dart
@@ -0,0 +1,4 @@
+library bwu_utils_browser.math;
+
+export 'package:bwu_utils/math/math.dart';
+
---
+++ lib/math/parse_num.dart
@@ -0,0 +1,4 @@
+library bwu_utils_browser.parse_num;
+
+export 'package:bwu_utils/math/parse_num.dart';
+
